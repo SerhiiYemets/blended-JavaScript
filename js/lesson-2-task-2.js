04e361027587a599561f3d8d2ab502c1b55ec8dd
Serhii Yemets <Serhiiyemets2020@gmail.com>
2025-08-14 09:18:35 +0200

    add lesson js

---
+++ js/lesson-2-task-2.js
@@ -0,0 +1,12 @@
+const points = 500;
+
+if (points <= 500) { 
+    console.log('Level 1');
+} else if (points > 500) {
+    console.log('Level 2');
+} else if (points > 1500) {
+    console.log('Level 3');
+}
+else {
+    console.log('Level 4');
+}
