164b938ed5386e8b4d55064eb7b4542f609c60ff
Serhii Yemets <Serhiiyemets2020@gmail.com>
2025-09-04 18:29:27 +0200

    add js file

--- js/others.js
+++ js/others.js
@@ -731,3 +731,138 @@
 // }
 // console.log(calc(10, 20, (x, y) => x + y));
 
+
+
+// const calculate = (...arguments) => {
+//   let total = 0;
+//   arguments.forEach(item => total += item); 
+//   return total; 
+// }
+
+// console.log(calculate(1, 2, 3, 4, 5)); // 15
+
+
+
+
+/**
+ * Array.prototype.map()
+ * - Поелементо перебирає оригінальний масив
+ * - Не змінює оригінальний масив
+ * - Повертає новий масив такої ж довжини
+ */
+
+// const allCars = [
+//   { make: "Honda", model: "CR-V", amount: 14, price: 24045 },
+//   { make: "Honda", model: "Accord", amount: 2, price: 22455 },
+//   { make: "Mazda", model: "Mazda 6", amount: 8, price: 24195 },
+//   { make: "Mazda", model: "CX-9", amount: 7, price: 31520 },
+//   { make: "Toyota", model: "4Runner", amount: 19, price: 34210 },
+//   { make: "Toyota", model: "Sequoia", amount: 16, price: 45560 },
+//   { make: "Toyota", model: "Tacoma", amount: 4, price: 24320 },
+//   { make: "Ford", model: "F-150", amount: 11, price: 27110 },
+//   { make: "Ford", model: "Fusion", amount: 13, price: 22120 },
+//   { make: "Ford", model: "Explorer", amount: 6, price: 31660 },
+// ];
+
+/**
+* Нехай функція getModels повертає масив моделей (поле model) всіх автомобілів.
+*/
+
+// const getModels = (cars) => {
+//     return cars.map((cars) => cars.model)
+// };
+
+// console.log(getModels(allCars));
+
+
+/**
+* Нехай функція getModels повертає масив (поле price) всіх автомобілів.
+*/
+
+
+// const getModels = (priceModels) => {
+//   return priceModels.map((priceModels) => priceModels.price)
+// }
+// console.log(getModels(allCars));
+
+
+
+// 
+/**
+* Нехай функція makeCarsWithDiscount повертає новий масив об'єктів із змінним
+* значенням властивості price залежно від переданої знижки.
+*/
+
+// const makeCarsWithDiscount = (cars, discount) => {
+//   return cars.map((car) => {
+//     return {
+//       ...car,
+//       price: car.price * (1 - discount)
+//       }
+//     })
+// }
+
+
+// console.table(makeCarsWithDiscount(allCars, 0.2));
+// console.table(makeCarsWithDiscount(allCars, 0.4));
+// console.table(makeCarsWithDiscount(allCars, 0.6));
+
+
+
+
+
+
+
+const players = [
+    { id: "player-1", name: "Mango", timePlayed: 310, points: 54, online: false },
+    { id: "player-2", name: "Poly", timePlayed: 470, points: 92, online: true },
+    { id: "player-3", name: "Kiwi", timePlayed: 230, points: 48, online: true },
+    { id: "player-4", name: "Ajax", timePlayed: 150, points: 71, online: false },
+    { id: "player-5", name: "Chelsy", timePlayed: 80, points: 48, online: true },
+];
+
+/*
+* Збільшуємо кількість годин гравця за id
+// */
+// const playerId = "player-4";
+
+
+// const newPlayers = players.map((item) => {
+//     if(item.id === playerId) {
+//         return {
+//             ...item,
+//             timePlayed: item.timePlayed + 100
+//         }
+//     }
+
+//     return item;
+// })
+
+// console.table(newPlayers);
+
+
+
+
+
+
+
+
+// ------------------------------- 5
+
+/**
+* Метод flatMap
+*/
+
+// const tweets = [
+//     { id: "000", likes: 5, tags: ["js", "nodejs"] },
+//     { id: "001", likes: 2, tags: ["html", "css"] },
+//     { id: "002", likes: 17, tags: ["html", "js", "nodejs"] },
+//     { id: "003", likes: 8, tags: ["css", "react"] },
+//     { id: "004", likes: 0, tags: ["js", "nodejs", "react"] },
+// ];
+
+
+
+// const tags = tweets.flatMap((tweet) => tweet.tags);
+
+// console.log(tags);
