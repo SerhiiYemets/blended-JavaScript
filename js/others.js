81cfa1c60f253b285a4f272937d44de2d25b728d
Serhii Yemets <Serhiiyemets2020@gmail.com>
2025-09-04 19:42:55 +0200

    add js file

--- js/others.js
+++ js/others.js
@@ -810,16 +810,13 @@
 
 
 
-
-
-
-const players = [
-    { id: "player-1", name: "Mango", timePlayed: 310, points: 54, online: false },
-    { id: "player-2", name: "Poly", timePlayed: 470, points: 92, online: true },
-    { id: "player-3", name: "Kiwi", timePlayed: 230, points: 48, online: true },
-    { id: "player-4", name: "Ajax", timePlayed: 150, points: 71, online: false },
-    { id: "player-5", name: "Chelsy", timePlayed: 80, points: 48, online: true },
-];
+// const players = [
+//     { id: "player-1", name: "Mango", timePlayed: 310, points: 54, online: false },
+//     { id: "player-2", name: "Poly", timePlayed: 470, points: 92, online: true },
+//     { id: "player-3", name: "Kiwi", timePlayed: 230, points: 48, online: true },
+//     { id: "player-4", name: "Ajax", timePlayed: 150, points: 71, online: false },
+//     { id: "player-5", name: "Chelsy", timePlayed: 80, points: 48, online: true },
+// ];
 
 /*
 * Збільшуємо кількість годин гравця за id
@@ -841,12 +838,6 @@ const players = [
 // console.table(newPlayers);
 
 
-
-
-
-
-
-
 // ------------------------------- 5
 
 /**
@@ -862,7 +853,43 @@ const players = [
 // ];
 
 
+// метод flatMap
 
 // const tags = tweets.flatMap((tweet) => tweet.tags);
 
 // console.log(tags);
+
+
+// const numbers = [5, 10, 15, 20, 25];
+// const res = numbers.filter((item) => item > 10;
+// );
+// console.log(res);
+
+
+// const allCars = [
+//   { make: "Honda", model: "CR-V", type: "suv", amount: 14, price: 24045, onSale: true},
+//   { make: "Honda", model: "Accord", type: "sedan", amount: 2, price: 22455, onSale: true },
+//   { make: "Mazda", model: "Mazda 6", type: "sedan", amount: 8, price: 24195, onSale: false },
+//   { make: "Mazda", model: "CX-9", type: "suv", amount: 7, price: 31520, onSale: true },
+//   { make: "Toyota", model: "4Runner", type: "suv", amount: 19, price: 34210, onSale: false },
+//   { make: "Toyota", model: "Sequoia", type: "suv", amount: 16, price: 45560, onSale: false },
+//   { make: "Toyota", model: "Tacoma", type: "truck", amount: 4, price: 24320, onSale: true },
+//   { make: "Ford", model: "F-150", type: "truck", amount: 11, price: 27110, onSale: true },
+//   { make: "Ford", model: "Fusion", type: "sedan", amount: 13, price: 22120, onSale: true },
+//   { make: "Ford", model: "Explorer", type: "suv", amount: 6, price: 31660, onSale: false }
+// ];
+
+
+// метод filter
+// const filterByPrice = (cars, threshold) => {
+//   return cars.filter((car) => car.price < threshold)
+// }
+// console.table(filterByPrice(allCars, 30000));
+
+
+// метод find
+
+
+// const numbers = [5, 10, 15, 20, 25];
+// const res = numbers.find((item) => item > 10);
+// console.log(res);
