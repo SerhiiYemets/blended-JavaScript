47e00ae53e2312ccf0146296637a50c1cf5513d5
Serhii Yemets <Serhiiyemets2020@gmail.com>
2025-09-17 02:02:59 +0200

    add js file

--- js/others.js
+++ js/others.js
@@ -1328,5 +1328,24 @@
 // console.log(alice);
 
 
+// Деструктуризація в циклах
 
+// const books = [
+//   {
+//     title: "The Last Kingdom",
+//     author: "Bernard Cornwell",
+//     rating: 8.38,
+//   },
+//   {
+//     title: "Beside Still Waters",
+//     author: "Robert Sheckley",
+//     rating: 8.51,
+//   },
+// ];
+
+// for (const book of books) {
+//   console.log(book.title);
+//   console.log(book.author);
+//   console.log(book.rating);
+// }
 
