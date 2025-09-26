0febf68dc776c1cc6fbb4caca5854ad901d634d5
Serhii Yemets <Serhiiyemets2020@gmail.com>
2025-09-26 21:35:26 +0200

    add js file

--- js/lesson-4.js
+++ js/lesson-4.js
@@ -21,105 +21,105 @@
 // 19 - зроби це саме, але використовуй шаблонні рядки та метод insertAdjacentHTML()
 // 20 - очисти список
 
-// 1)
-const bodyEl = document.querySelector("body");
-console.log(bodyEl);
-// 2)
-const titleEl = document.querySelector("#title");
-console.log(titleEl);
-// 3)
-const listEl = document.querySelector(".list");
-console.log(listEl);
-// 4)
-const dataEl = document.querySelectorAll("[data-topic]");
-console.log(dataEl);
-// 5)
-console.log(dataEl[0]);
-// 6)
-console.log(dataEl[dataEl.length - 1]);
-// 7)
-const h1El = document.querySelector("h1");
-console.log(h1El.nextElementSibling);
-// 8)
-const h3El = document.querySelectorAll("h3");
-console.log(h3El);
-// 9)
-h3El.forEach(el => el.classList.add("active"));
-console.log(h3El);
-// 10)
-const liEl = document.querySelector('li[data-topic="navigation"]');
-console.log(liEl);
-// 11)
-const liEls = document.querySelectorAll('li[data-topic="navigation"]');
-liEls.forEach(el => {
-    el.style.backgroundColor = "yellow";
-});
-// 12)
-const liElement = document.querySelector('li[data-topic="navigation"]');
-if (liElement) {
-    const pLiEl = liElement.querySelector("p");
-    if (pLiEl) {
-        pLiEl.textContent = "Я змінив тут текст!";
-    };
-}; 
-// 13)
-const currentTopic = "manipulation"
-const dataEls = document.querySelector(`[data-topic="${currentTopic}"]`);
-console.log(dataEls);
-// 14)
-const dataElement = document.querySelector(`[data-topic="${currentTopic}"]`);
-dataElement.style.backgroundColor = "lightblue";
-// 15)
-const hEl = document.querySelector('h3.completed');
-if (hEl) {
-    console.log(hEl);
-} else {
-    console.log(`Елемент 'h3.completed' не знайдено`);
-}
-//16)
-document.querySelector('h3.completed').closest('li').remove();
+// // 1)
+// const bodyEl = document.querySelector("body");
+// console.log(bodyEl);
+// // 2)
+// const titleEl = document.querySelector("#title");
+// console.log(titleEl);
+// // 3)
+// const listEl = document.querySelector(".list");
+// console.log(listEl);
+// // 4)
+// const dataEl = document.querySelectorAll("[data-topic]");
+// console.log(dataEl);
+// // 5)
+// console.log(dataEl[0]);
+// // 6)
+// console.log(dataEl[dataEl.length - 1]);
+// // 7)
+// const h1El = document.querySelector("h1");
+// console.log(h1El.nextElementSibling);
+// // 8)
+// const h3El = document.querySelectorAll("h3");
+// console.log(h3El);
+// // 9)
+// h3El.forEach(el => el.classList.add("active"));
+// console.log(h3El);
+// // 10)
+// const liEl = document.querySelector('li[data-topic="navigation"]');
+// console.log(liEl);
+// // 11)
+// const liEls = document.querySelectorAll('li[data-topic="navigation"]');
+// liEls.forEach(el => {
+//     el.style.backgroundColor = "yellow";
+// });
+// // 12)
+// const liElement = document.querySelector('li[data-topic="navigation"]');
+// if (liElement) {
+//     const pLiEl = liElement.querySelector("p");
+//     if (pLiEl) {
+//         pLiEl.textContent = "Я змінив тут текст!";
+//     };
+// }; 
+// // 13)
+// const currentTopic = "manipulation"
+// const dataEls = document.querySelector(`[data-topic="${currentTopic}"]`);
+// console.log(dataEls);
+// // 14)
+// const dataElement = document.querySelector(`[data-topic="${currentTopic}"]`);
+// dataElement.style.backgroundColor = "lightblue";
+// // 15)
+// const hEl = document.querySelector('h3.completed');
+// if (hEl) {
+//     console.log(hEl);
+// } else {
+//     console.log(`Елемент 'h3.completed' не знайдено`);
+// }
+// //16)
+// document.querySelector('h3.completed').closest('li').remove();
 
-// const h3 = document.querySelector('h3.completed'); 
-// const li = h3 && h3.closest('li');
-// if (li) li.remove(); 
-// 17)
-const navP = document.querySelector("h1");
-const pEl = document.createElement("p");
-pEl.textContent = "Об'єктна модель документа (Document Object Model)";
-navP.insertAdjacentElement("afterend", pEl);
-console.log(pEl);
-// 18)
+// // const h3 = document.querySelector('h3.completed'); 
+// // const li = h3 && h3.closest('li');
+// // if (li) li.remove(); 
+// // 17)
+// const navP = document.querySelector("h1");
+// const pEl = document.createElement("p");
+// pEl.textContent = "Об'єктна модель документа (Document Object Model)";
+// navP.insertAdjacentElement("afterend", pEl);
+// console.log(pEl);
+// // 18)
 
-const navEl = document.querySelector(".list");
-const navItemEL = document.createElement("li");
-const navH3 = document.createElement("h3");
-navH3.textContent = "Властивість innerHTML";
-const navPel = document.createElement("p");
-navPel.textContent = "Ще один спосіб створити DOM-елементи і помістити їх в дерево - це використовувати рядки з тегами і дозволити браузеру зробити всю важку роботу";
+// const navEl = document.querySelector(".list");
+// const navItemEL = document.createElement("li");
+// const navH3 = document.createElement("h3");
+// navH3.textContent = "Властивість innerHTML";
+// const navPel = document.createElement("p");
+// navPel.textContent = "Ще один спосіб створити DOM-елементи і помістити їх в дерево - це використовувати рядки з тегами і дозволити браузеру зробити всю важку роботу";
 
-navItemEL.appendChild(navH3);
-navItemEL.appendChild(navPel);
-navEl.appendChild(navItemEL);
+// navItemEL.appendChild(navH3);
+// navItemEL.appendChild(navPel);
+// navEl.appendChild(navItemEL);
 
-console.log(navItemEL);
+// console.log(navItemEL);
 
-// 19)
-const navItemEl = document.querySelector(".list");
+// // 19)
+// const navItemEl = document.querySelector(".list");
 
-const navElHtml = `
-    <li>
-        <h3>Властивість innerHTML</h3>
-        <p>Ще один спосіб створити DOM-елементи і помістити їх в дерево - це використовувати рядки з тегами і дозволити браузеру зробити всю важку роботу</p>
-    </li>
-`;
+// const navElHtml = `
+//     <li>
+//         <h3>Властивість innerHTML</h3>
+//         <p>Ще один спосіб створити DOM-елементи і помістити їх в дерево - це використовувати рядки з тегами і дозволити браузеру зробити всю важку роботу</p>
+//     </li>
+// `;
 
-navEl.insertAdjacentHTML("afterbegin", navElHtml);
+// navEl.insertAdjacentHTML("afterbegin", navElHtml);
 
-console.log(navItemEl);
+// console.log(navItemEl);
 
-// 20)
-const navElDelet = document.querySelector(".list");
-navEl.innerHTML = "";
+// // 20)
+// const navElDelet = document.querySelector(".list");
+// navEl.innerHTML = "";
 
 
 
@@ -152,11 +152,26 @@ for (let i = 0; i < 100; i++) {
 
 // Form Events, Input, Focus, Blur and Submit.
 
-// Використовуй шаблон форми з файлу html.
+// // Використовуй шаблон форми з файлу html.
 
-// 1 - При події `input`, якщо користувач ввів в поле більше 
-// 6 символів то додати клас `success`. Якщо ж символів менше аніж 6,
-// то клас `error`
+// // 1 - При події `input`, якщо користувач ввів в поле більше 
+// // 6 символів то додати клас `success`. Якщо ж символів менше аніж 6,
+// // то клас `error`
+
+// // 1. Знайти елемент <input> за ID
+// const inputElement = document.getElementById('username');
+
+// inputElement.addEventListener('input', (event) => {
+//     const value = event.target.value;
+//     if (value.length >= 6) {
+//         inputElement.classList.add('success');
+//         inputElement.classList.remove('error');
+//     } else {
+//         inputElement.classList.add('error');
+//         inputElement.classList.remove('success');
+//     }
+// });
+
 
 // 2 - При події `focus` зроби перевірку на пустоту поля інпута,
 // якщо ж поле пусте, то зроби `outline` => `'3px solid red'`,
@@ -183,5 +198,37 @@ for (let i = 0; i < 100; i++) {
 // Завдання 4:
 
  // Використовуй шаблон розмітки з файлу html та напиши наступний функціонал:
- // При кліку на кнопку "Зменшити" квадрат стає меньшим на 20 пікселів, 
- // При кліку на кнопку "Збільшити" - квадрат стає більшим на 20 пікселів.
+ // При кліку на кнопку "Зменшити" квадрат стає меньшим на 20 пікселів,
+// При кліку на кнопку "Збільшити" - квадрат стає більшим на 20 пікселів.
+ 
+
+// Завдання 5
+// При натисканні на кожну з кнопок підсумовуються значення з data-атрибутів.
+// За натисканням на кнопку "Вивести результат" виводиться сума значення, а також статистика з
+// інформацією про те, яка кнопка була натиснута скільки разів.
+// */
+
+
+// const statList = document.querySelector('.statList')
+// const resultSection = document.querySelector('#resultSection')
+// const resultButton = document.querySelector('#resultButton')
+
+// let sumNumber = 0;
+
+// statList.addEventListener('click', onStatListItemClick);
+
+
+// function onStatListItemClick(event) {
+//     if (event.target.nodeName !== 'BUTTON') {
+//         return;
+//     }
+//     const dataNumber = Number(event.target.dataset.number);
+//     sumNumber += dataNumber;
+//     console.log(dataNumber); 
+// }
+
+// function onResultButtonClick() {
+//     resultSection.innerHTML = `Сумма значень з data-антрибутів ${sumNumber}`;
+//     sumNumber = 0;
+// }
+
