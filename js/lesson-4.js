31bcbf35b72391beffc8536dd93164eb622fd953
Serhii Yemets <Serhiiyemets2020@gmail.com>
2025-10-01 10:07:41 +0200

    add js file

--- js/lesson-4.js
+++ js/lesson-4.js
@@ -316,3 +316,22 @@
 //     sumNumber = 0;
 // }
 
+
+// Дата та час
+
+// const date = new Date();
+// console.log(date.getTime());
+
+
+
+const startTime = Date.now();
+// Твій код, виконуваний упродовж деякого часу
+for(let i = 0; i <= 100; i += 1) {
+	console.log(i);
+}
+
+const endTime = Date.now();
+const elapsedTime = endTime - startTime;
+
+console.log(`Elapsed time: ${elapsedTime} ms`);
+
