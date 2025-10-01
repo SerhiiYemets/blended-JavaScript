9b6cc481e0a63bcbebbc7b1e6fd0063931de231b
Serhii Yemets <Serhiiyemets2020@gmail.com>
2025-10-01 11:10:31 +0200

    add js file

--- js/lesson-4.js
+++ js/lesson-4.js
@@ -324,14 +324,74 @@
 
 
 
-const startTime = Date.now();
-// Твій код, виконуваний упродовж деякого часу
-for(let i = 0; i <= 100; i += 1) {
-	console.log(i);
-}
+// const startTime = Date.now();
+// // Твій код, виконуваний упродовж деякого часу
+// for(let i = 0; i <= 100; i += 1) {
+// 	console.log(i);
+// }
 
-const endTime = Date.now();
-const elapsedTime = endTime - startTime;
+// const endTime = Date.now();
+// const elapsedTime = endTime - startTime;
 
-console.log(`Elapsed time: ${elapsedTime} ms`);
+// console.log(`Elapsed time: ${elapsedTime} ms`);
+
+
+// Гетери та сетери
+
+// const date = new Date("March 16, 2030 14:25:00");
+// console.log("Date: ", date); // "Sat Mar 16 2030 14:25:00 GMT+0200"
+
+// // Повертає день місяця від 1 до 31
+// console.log("Day: ", date.getDate()); // 16
+
+// // Повертає день тижня від 0 до 6, починається з неділі
+// console.log("Day of the week: ", date.getDay()); // 6
+
+// // Повертає місяць від 0 до 11
+// console.log("Month: ", date.getMonth()); // 2
+
+// // Повертає рік з 4 цифр
+// console.log("Full year: ", date.getFullYear()); // 2030
+
+// // Повертає години
+// console.log("Hours: ", date.getHours()); // 14
+
+// // Повертає хвилини
+// console.log("Minutes: ", date.getMinutes()); // 25
+
+// // Повертає секунди
+// console.log("Seconds: ", date.getSeconds()); // 0
+
+// // Повертає мілісекунди
+// console.log("Milliseconds: ", date.getMilliseconds()); // 0
+
+
+// const date = new Date("March 16, 2030 14:25:00");
+
+// date.setMinutes(50); 
+// console.log(date); // "Sat Mar 16 2030 14:50:00 GMT+0200"
+
+// date.setFullYear(2040);
+// console.log(date); // "Fri Mar 16 2040 14:50:00 GMT+0200"
+
+// date.setMonth(4); 
+// console.log(date); // "Wed May 16 2040 14:50:00 GMT+0300"
+
+
+// ПРОМИС
+
+// const isSuccess = true;
+
+// const promise = new Promise((resolve, reject) => {
+//   setTimeout(() => {
+//     if (isSuccess) {
+//       resolve("Success! Value passed to resolve function");
+//     } else {
+//       reject("Error! Error passed to reject function");
+//     }
+//   }, 2000);
+// });
+
+// console.log(promise); // Об'єкт промісу
+
 
